cb91152769b20546ab4c14ce7af55a048fc5c783
Andalexshap <andalexshap@yandex.ru>
2022-11-30 17:44:27 +0300

    add gitignore. Add SelectingElementsByLength method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Программа примет данные и выдаст в резальтате только те, которые соответствуют условию.");
+
+int number = InputNumber("Введите количество строк, которое необходимо принять:");
+
+string[] inputData = InputStringToArray(number);
+
+string[] selectingInputDataByLenght = SelectingElementsByLength(inputData, 3);
+
+
+#region Methods
+int InputNumber(string message, bool number = false)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = int.TryParse(Console.ReadLine(), out int value);
+
+        if (!result)
+        {
+            Console.WriteLine($"Введены некоректные данные. {message} еще раз!");
+            Thread.Sleep(1500);
+            Console.Clear();
+
+            continue;
+        }
+
+        return value;
+    }
+}
+
+string[] InputStringToArray(int size)
+{
+    string[] inputData = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        inputData[i] = Console.ReadLine() ?? "null";
+    }
+
+    return inputData;
+}
+
+string[] SelectingElementsByLength(string[] array, int lenght)
+{
+    string[] selectingArray = array.Where(x => x.Length <= lenght).ToArray();
+    return selectingArray;
+}
+#endregion
