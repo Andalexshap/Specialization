4ae89ab6f9f1f4fd9319a03a6174baf73ea3d8db
Andalexshap <andalexshap@yandex.ru>
2022-11-30 17:56:02 +0300

    completion programm

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿Console.WriteLine("Программа примет данные и выдаст в резальтате только те, которые соответствуют условию.");
 
-int number = InputNumber("Введите количество строк, которое необходимо принять:");
+int number = InputNumber("Введите количество строк, которое необходимо принять: ");
 
 string[] inputData = InputStringToArray(number);
 
-string[] selectingInputDataByLenght = SelectingElementsByLength(inputData, 3);
+Console.WriteLine("Принятые данные:");
+PrintArray(inputData);
+
+int maxLenght = 3;
+string[] selectingInputDataByLenght = SelectingElementsByLength(inputData, maxLenght);
+
+Console.WriteLine($"Элементы, длина которых меньше или равна {maxLenght} символа:");
+PrintArray(selectingInputDataByLenght);
 
 
 #region Methods
@@ -33,8 +40,11 @@ string[] InputStringToArray(int size)
     string[] inputData = new string[size];
     for (int i = 0; i < size; i++)
     {
+        Console.Write($"Введите {i + 1}й элемент: ");
         inputData[i] = Console.ReadLine() ?? "null";
     }
+
+    Console.WriteLine();
 
     return inputData;
 }
@@ -43,5 +53,15 @@ string[] SelectingElementsByLength(string[] array, int lenght)
 {
     string[] selectingArray = array.Where(x => x.Length <= lenght).ToArray();
     return selectingArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.WriteLine($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
 }
 #endregion
